9bd1b48d98d910c2b0e046e4eb24c7111bab1dcb
Mikhail Fominykh <unit.misha@yandex.ru>
2022-11-23 01:00:24 +0300

    Task 3.1 update 12

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,7 +2,8 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
-
+   
+    product.item = {id: 1, title: "Пицца", count: 5, price: "500.00"};
     let itemCountText = " ";
     itemCountText += product.count;
     itemCountText += ` × `;
